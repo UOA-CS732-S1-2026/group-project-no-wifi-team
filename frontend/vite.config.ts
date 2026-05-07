ff29905051cb5dafbfd365581662001a0c02234e
GraceLiao77 <liaojin111@gmail.com>
2026-05-07 16:37:24 +1200

    chore: add project frontend deploy script

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/group-project-no-wifi-team/',
   plugins: [tailwindcss(), react()],
   server: {
     proxy: {
