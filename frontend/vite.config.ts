9592f3d2486748549aa81d920f827b16bdb74c05
GraceLiao77 <liaojin111@gmail.com>
2026-05-07 16:45:57 +1200

    chore: fix base value

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,8 +3,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig({
-  base: '/group-project-no-wifi-team/',
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/group-project-no-wifi-team/' : '/',
   plugins: [tailwindcss(), react()],
   server: {
     proxy: {
@@ -19,4 +19,4 @@ export default defineConfig({
     globals: true,
     setupFiles: ['./src/test/setup.ts'],
   },
-})
+}))
